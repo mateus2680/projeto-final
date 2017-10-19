35d7c9195e415390f04eea056dc13e4bba776ae3
Adriel Sales <adriel@abraao.local>
2017-10-19 21:48:30 -0200

    Organizando o projeto em pastas e adicionando includes no layout

--- categorias/form-categoria.php
+++ categorias/form-categoria.php
@@ -1,16 +1,7 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="utf-8">
-  <title>Cadastrar Nova Categoria</title>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-</head>
-<body>
-  <div class="container">
-    <a href="index.php">
-      <span class="glyphicon glyphicon-home" aria-hidden="true"></span>
-      Home
-    </a>
+<?php require_once("../includes/header-pages.php"); ?>
+
+<div class="container">
+
     <h3>Cadastrar Nova Categoria</h3>
     <form action="create-categoria.php" method="post">
       <div class="form-group">
@@ -33,5 +24,6 @@
       </div>
     </form>
   </div>
-</body>
-</html>
+</div>
+
+<?php require_once("../includes/footer-pages.php"); ?>
--- index.php
+++ index.php
@@ -1,21 +1,12 @@
-<!DOCTYPE html>
+<?php require_once("includes/header.php"); ?>
+<?php require_once("includes/menu.php"); ?>
 
-<html>
-<head>
-  <meta charset="utf-8">
-  <title>Loja Programador Web - Dashboard</title>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-</head>
-<body>
+<!-- conteudo -->
   <div class="container">
     <h3 class="alert alert-info">Loja Programador Web - Dashboard</h3>
     <hr>
-    <ul>
-      <li><a href="form-categoria.php">Nova Categoria</a></li>
-      <li><a href="listar-categorias.php">Listar Categorias</a></li>
-
-    </ul>
 
   </div>
-</body>
-</html>
+<!-- conteudo -->
+
+<?php require_once("includes/footer.php"); ?>
